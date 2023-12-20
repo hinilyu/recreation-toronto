a1a57f2662c125cbd8c6fafa3925942ee44050e8
hinilyu <hinilyu@gmail.com>
2023-12-19 23:22:45 -0500

    added cron job

--- app/api/scheduled-task/route.js
+++ app/api/scheduled-task/route.js
@@ -1,8 +1,5 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  if (req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`) {
-    return res.status(401).end("Unauthorized");
-  }
+export async function GET(req) {
   return NextResponse.json({ ok: true });
 }
